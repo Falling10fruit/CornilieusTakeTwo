f964c2deaabb9101ed5f4cbf42890b7cd8a3778a
Falling10fruit <budiyoga09nur@gmail.com>
2025-09-09 01:06:09 +0700

    renderWorld.ts

--- src/ts/setUp.ts
+++ src/ts/setUp.ts
@@ -2,6 +2,7 @@ import * as canvasResize from "./prerequisites/canvasResize"
 import { loadSpritesheet } from "./prerequisites/loadSpritesheet";
 import { setUpGPU } from "./prerequisites/setUpGPU";
 import { setUpRender, render} from "./render/render.ts";
+import { setUpRenderWorld, bindWorldStorageBuffer } from "./render/renderWorld.ts";
 
 try {
     const { device, ctx } = await setUpGPU();
@@ -14,8 +15,8 @@ try {
     window.world.storageBuffer = window.generateWorld.generateWorldStorageBuffer(window.world);
     window.generateWorld.generateWorldToBuffer({...window.world, worldBuffer: window.world.storageBuffer});
 
-    window.renderWorld.setUp(device);
-    window.renderWorld.bindWorldStorageBuffer({...window.world, worldBuffer: window.world.storageBuffer});
+    setUpRenderWorld({ device });
+    bindWorldStorageBuffer({...window.world, worldBuffer: window.world.storageBuffer});
 
 
     render(); // render and gametick are not synced
