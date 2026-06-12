a77ee3cb8b4f69138e075c4c5e3b66b9da72eaf1
Falling10fruit <budiyoga09nur@gmail.com>
2026-06-12 17:52:47 +0700

    compute iterations now at least consistently continue when not in debugging mode

--- src/ts/compute/compute.ts
+++ src/ts/compute/compute.ts
@@ -9,7 +9,7 @@ function setUpComputePass (parameters: { device: GPUDevice}) {
     device = parameters.device;
 }
 
-const debugging_time = true;
+const debugging_time = false;
 let debugging_loops = 999999999;
 function compute() {
     // if (debugging_time) window.keyIsDown["w"] = true;
@@ -24,38 +24,42 @@ function compute() {
     computeEntities(computePass);
     computePass.end();
     
-    if (window.debug.buffer == null)        return window.fail({title: `debug buffer is null`,        message: `debugging entities`});
-    if (window.debug.unmapped_buffers == null) return window.fail({title: `debug unmapped buffers is null`, message: `debugging entities`});
-    let mapping_buffer = null;
-    if (window.debug.unmapped_buffers.length > 0) {
-        mapping_buffer = window.debug.unmapped_buffers[0];
-        window.debug.unmapped_buffers.splice(0, 1);
-    }
-    if (mapping_buffer == null) return;
-    if (mapping_buffer != null && window.debug.unmapped_buffers.length > 0) commandEncoder.copyBufferToBuffer(window.debug.buffer, mapping_buffer);
+    if (debugging_time) {
+        if (window.debug.buffer == null)        return window.fail({title: `debug buffer is null`,        message: `debugging entities`});
+        if (window.debug.unmapped_buffers == null) return window.fail({title: `debug unmapped buffers is null`, message: `debugging entities`});
+        let mapping_buffer = null;
+        if (window.debug.unmapped_buffers.length > 0) {
+            mapping_buffer = window.debug.unmapped_buffers[0];
+            window.debug.unmapped_buffers.splice(0, 1);
+        }
+        if (mapping_buffer == null) return;
+        if (mapping_buffer != null && window.debug.unmapped_buffers.length > 0) commandEncoder.copyBufferToBuffer(window.debug.buffer, mapping_buffer);
+        
+        device.queue.submit([commandEncoder.finish()]);
     
-    device.queue.submit([commandEncoder.finish()]);
-
-    if (mapping_buffer != null && window.debug.unmapped_buffers.length > 0) {
-        mapping_buffer.mapAsync(GPUMapMode.READ).then(function () {
-            if (window.debug.unmapped_buffers == null) return window.fail({title: `debug unmapped buffers is null`, message: `debug unmapped buffers became null after trying to map it for reading whilst debugging entities`});
-
-            // console.log(`debug reference`)
-            // print_bits(8388802);
-            // console.log("debug buffer");
-            // print_bits((new Uint32Array(mapping_buffer.getMappedRange()))[0]);
-            console.log((new Uint32Array(mapping_buffer.getMappedRange()))[0]);
-            // console.log((new Float32Array(mapping_buffer.getMappedRange()))[0]);
-            // console.log((new Int32Array(mapping_buffer.getMappedRange()))[0]);
-            mapping_buffer.unmap();
-            
-            window.debug.unmapped_buffers.push(mapping_buffer);
-        });
+        if (mapping_buffer != null && window.debug.unmapped_buffers.length > 0) {
+            mapping_buffer.mapAsync(GPUMapMode.READ).then(function () {
+                if (window.debug.unmapped_buffers == null) return window.fail({title: `debug unmapped buffers is null`, message: `debug unmapped buffers became null after trying to map it for reading whilst debugging entities`});
+    
+                // console.log(`debug reference`)
+                // print_bits(8388802);
+                // console.log("debug buffer");
+                // print_bits((new Uint32Array(mapping_buffer.getMappedRange()))[0]);
+                console.log((new Uint32Array(mapping_buffer.getMappedRange()))[0]);
+                // console.log((new Float32Array(mapping_buffer.getMappedRange()))[0]);
+                // console.log((new Int32Array(mapping_buffer.getMappedRange()))[0]);
+                mapping_buffer.unmap();
+                
+                window.debug.unmapped_buffers.push(mapping_buffer);
+            });
+        }
+    } else {
+        device.queue.submit([commandEncoder.finish()]);
     }
     
+    console.log("will request animation frame", debugging_loops > 0 || !debugging_time);
     if (debugging_loops > 0 || !debugging_time) {
         debugging_loops--;
-        console.log("debugging loops left", debugging_loops);
         window.world.entities.current_entity_buffer_is = (window.world.entities.current_entity_buffer_is == 0) ? 1 : 0;
         requestAnimationFrame(compute) // eventually transfer to webworkers
     };
